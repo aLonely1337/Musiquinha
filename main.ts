67033a2331f331de732ac706db0bbdf6bcd34e37
aLonely1337 <63696872+aLonely1337@users.noreply.github.com>
2021-03-18 10:37:52 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,5 @@
+for (let index = 0; index < 5; index++) {
+    music.playMelody("C5 D F F G A B G ", 95)
+    music.playMelody("C5 D E F A G E B ", 120)
+    music.playMelody("C5 B B A A B B C5 ", 143)
+}
